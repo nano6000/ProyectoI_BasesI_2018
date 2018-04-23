72cd472190d5f5187c1b7f5dc77d96627a9bef31
Esteban Bosques <nano.95.eb@gmail.com>
2018-04-23 15:22:39 -0600

    Diagramas logico y relacional. Cambios en la interfaz

---
+++ InterfazHuerta/info.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php 
+	// Create connection to Oracle
+$conn = oci_connect("hr", "shinigami", "localhost/orcl");
+if (!$conn) {
+   $m = oci_error();
+   echo $m['message'], "\n";
+   exit;
+}
+else {
+   echo "Connected to Oracle!";
+   header("Location:home.html")
+}
+// Close the Oracle connection
+oci_close($conn);
+?>
+
+</body>
+</html>
