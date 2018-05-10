2a38fa1c57a9228fa55f64c567467f33d3370634
Esteban Bosques <nano.95.eb@gmail.com>
2018-05-09 23:23:26 -0600

    arreglos para el registro de usuarios

--- InterfazHuerta/scripts/form-input.php
+++ InterfazHuerta/scripts/form-input.php
@@ -23,32 +23,69 @@ include_once('conexion.inc');
         $passwd =$_POST["passwd-input"];
         $img = file_get_contents($_FILES['customFile']['tmp_name']);
 
-        //comprobar que no exista el Usuario
-
-        $lob = oci_new_descriptor($conn, OCI_D_LOB);
-        $sql = "BEGIN people_management.insert_persona(:cedula, :pNombre, :sNombre, :pApellido, :sApellido,
-                        :fechNac, :img, :pais, :pEmail, :pTel, :usuario, :passwd); END;";
+        $result = 0;
+        $sql = "BEGIN :result := people_management.check_person(:cedula); END;";
         $s = oci_parse($conn, $sql);
-
-        oci_bind_by_name($s, ':img', $lob, -1, OCI_B_BLOB);
-        oci_bind_by_name($s,':pNombre',$pNombre);
-        oci_bind_by_name($s,':sNombre',$sNombre);
-        oci_bind_by_name($s,':pApellido',$pApellido);
-        oci_bind_by_name($s,':sApellido',$sApellido);
-        oci_bind_by_name($s,':pEmail',$pEmail);
-        oci_bind_by_name($s,':pTel',$pTel);
-        oci_bind_by_name($s,':pais',$pais);
-        oci_bind_by_name($s,':cedula',$cedula);
-        oci_bind_by_name($s,':fechNac',$fechNac);
-        oci_bind_by_name($s,':usuario',$usuario);
-        oci_bind_by_name($s,':passwd',$passwd);
-        $myv = file_get_contents($_FILES['customFile']['tmp_name']);
-        $lob->writeTemporary($myv, OCI_TEMP_BLOB);
+        oci_bind_by_name($s, ':cedula', $cedula);
+        oci_bind_by_name($s, ':result', $result);
         oci_execute($s);
-        $error =  oci_error($s);
-        echo htmlentities($error['message']);
 
-        header("Location: ../homeUser.php");
+        if($result>0)
+        {
+            echo "La persona ya existe";
+            header("Location: ../signUp.php?signup=duplicate&pNombre=$pNombre&sNombre=$sNombre&
+                        pApellido=$pApellido&sApellido=$sApellido&pEmail=$pEmail&sEmail=$sEmail&pTel=$pTel&
+                        sTel=$sTel&pais=$pais&usuario=$usuario");
+            exit();
+        }
+        else
+        {
+            //comprobar que no exista el Usuario
+            $result = 0;
+            $sql = "BEGIN :result := people_management.check_username(:usuario); END;";
+            $s = oci_parse($conn, $sql);
+            oci_bind_by_name($s, ':usuario', $usuario);
+            oci_bind_by_name($s, ':result', $result);
+            oci_execute($s);
+
+            if ($result>0)
+            {
+                echo "El usuario ya existe";
+                header("Location: ../signUp.php?signup=usernameTaken&pNombre=$pNombre&sNombre=$sNombre&
+                            pApellido=$pApellido&sApellido=$sApellido&pEmail=$pEmail&sEmail=$sEmail&pTel=$pTel&
+                            sTel=$sTel&pais=$pais&cedula=$cedula");
+                exit();
+            }
+            else
+            {
+                $lob = oci_new_descriptor($conn, OCI_D_LOB);
+                $sql = "BEGIN people_management.insert_persona(:cedula, :pNombre, :sNombre, :pApellido, :sApellido,
+                                :fechNac, :img, :pais, :pEmail, :pTel, :usuario, :passwd); END;";
+                $s = oci_parse($conn, $sql);
+
+                oci_bind_by_name($s, ':img', $lob, -1, OCI_B_BLOB);
+                oci_bind_by_name($s, ':pNombre',$pNombre);
+                oci_bind_by_name($s, ':sNombre',$sNombre);
+                oci_bind_by_name($s, ':pApellido',$pApellido);
+                oci_bind_by_name($s, ':sApellido',$sApellido);
+                oci_bind_by_name($s, ':pEmail',$pEmail);
+                oci_bind_by_name($s, ':pTel',$pTel);
+                oci_bind_by_name($s, ':pais',$pais);
+                oci_bind_by_name($s, ':cedula',$cedula);
+                oci_bind_by_name($s, ':fechNac',$fechNac);
+                oci_bind_by_name($s, ':usuario',$usuario);
+                oci_bind_by_name($s, ':passwd',$passwd);
+                $myv = file_get_contents($_FILES['customFile']['tmp_name']);
+                $lob->writeTemporary($myv, OCI_TEMP_BLOB);
+                oci_execute($s);
+                $error =  oci_error($s);
+                echo htmlentities($error['message']);
+
+                session_start();
+
+                header("Location: ../homeUser.php");
+            }
+        }
 
     }
 
--- InterfazHuerta/signUp.php
+++ InterfazHuerta/signUp.php
@@ -35,24 +35,64 @@ include_once('scripts/conexion.inc');
 				</div>
 				<div class="form-row">
 					<div class="form-group col-md-6">
-						<input name="frstName-input" type="text" class="form-control" placeholder="Primer Nombre" required>
+						<?php
+							if (isset($_GET['pNombre']))
+							{
+								$pNombre = $_GET['pNombre'];
+								echo '<input name="frstName-input" type="text" class="form-control" placeholder="Primer Nombre" value="'.$pNombre.'" required>';
+							}
+							else
+							{
+								echo '<input name="frstName-input" type="text" class="form-control" placeholder="Primer Nombre" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 						</div>
 					</div>
 					<div class="form-group col-md-6">
-						<input name="secndName-input"	type="text" class="form-control" placeholder="Segundo Nombre">
+						<?php
+							if (isset($_GET['sNombre']))
+							{
+								$sNombre = $_GET['sNombre'];
+								echo '<input name="secndName-input"	type="text" class="form-control" placeholder="Segundo Nombre" value="'.$sNombre.'">';
+							}
+							else
+							{
+								echo '<input name="secndName-input"	type="text" class="form-control" placeholder="Segundo Nombre">';
+							}
+						?>
 					</div>
 				</div>
 				<div class="form-row">
 					<div class="form-group col-md-6">
-						<input name="frstLstName-input" type="text" class="form-control" placeholder="Primer Apellido" required>
+						<?php
+							if (isset($_GET['pApellido']))
+							{
+								$pApellido = $_GET['pApellido'];
+								echo '<input name="frstLstName-input" type="text" class="form-control" placeholder="Primer Apellido" value="'.$pApellido.'" required>';
+							}
+							else
+							{
+								echo '<input name="frstLstName-input" type="text" class="form-control" placeholder="Primer Apellido" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 						</div>
 					</div>
 					<div class="form-group col-md-6">
-						<input name="secndLstName-input" type="text" class="form-control" placeholder="Segundo Apellido">
+						<?php
+							if (isset($_GET['sApellido']))
+							{
+								$sApellido = $_GET['sApellido'];
+								echo '<input name="secndLstName-input" type="text" class="form-control" placeholder="Segundo Apellido" value="'.$sApellido.'">';
+							}
+							else
+							{
+								echo '<input name="secndLstName-input" type="text" class="form-control" placeholder="Segundo Apellido">';
+							}
+						?>
 					</div>
 				</div>
 				<div class="form-row">
@@ -61,10 +101,19 @@ include_once('scripts/conexion.inc');
 						<select name="citizenship" name="citizenship" class="custom-select" required>
 							<option>Seleccione una opcion</option>
 							<?php
+								if (isset($_GET['pais']))
+									$pais = $_GET['pais'];
+								else
+									$pais = "";
 								$matriz = oci_parse($conn, "select * from ".$usuario.".Pais");
 								oci_execute($matriz);
 								while ($fila = oci_fetch_array($matriz, OCI_NUM+OCI_RETURN_NULLS))
-									echo "<option value=\"$fila[0]\">" . $fila[1] . " (" . $fila[0] . ")" . "</option>";
+								{
+									if ($fila[0] == $pais)
+										echo "<option value=\"$fila[0]\" selected>" . $fila[1] . " (" . $fila[0] . ")" . "</option>";
+									else
+										echo "<option value=\"$fila[0]\">" . $fila[1] . " (" . $fila[0] . ")" . "</option>";
+								}
 							?>
 						</select>
 						<div class="invalid-feedback">
@@ -73,7 +122,17 @@ include_once('scripts/conexion.inc');
 					</div>
 					<div class="form-group col-md-6">
 						<label>Cedula:</label>
-						<input name="id-input" type="text" class="form-control" pattern="[0-9]{9}" placeholder="Cedula" required>
+						<?php
+							if (isset($_GET['cedula']))
+							{
+								$cedula = $_GET['cedula'];
+								echo '<input name="id-input" type="text" class="form-control" pattern="[0-9]{9}" placeholder="Cedula" value="'.$cedula.'" required>';
+							}
+							else
+							{
+								echo '<input name="id-input" type="text" class="form-control" pattern="[0-9]{9}" placeholder="Cedula" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 							Ej. 123456789
@@ -83,27 +142,67 @@ include_once('scripts/conexion.inc');
 				<div class="form-row">
 					<div class="form-group col-md-6">
 						<label>E-mail 1:</label>
-						<input name="email1-input" type="text" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail 1" required>
+						<?php
+							if (isset($_GET['pEmail']))
+							{
+								$pEmail = $_GET['pEmail'];
+								echo '<input name="email1-input" type="text" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail 1" value="'.$pEmail.'" required>';
+							}
+							else
+							{
+								echo '<input name="email1-input" type="text" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail 1" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 						</div>
 					</div>
 					<div class="form-group col-md-6">
 						<label>E-mail 2:</label>
-						<input name="email2-input" type="email" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail">
+						<?php
+							if (isset($_GET['sEmail']))
+							{
+								$sEmail = $_GET['sEmail'];
+								echo '<input name="email2-input" type="email" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail 2" value="'.$sEmail.'">';
+							}
+							else
+							{
+								echo '<input name="email2-input" type="email" class="form-control" pattern=".+@.+\.(com|es)" placeholder="E-mail 2">';
+							}
+						?>
 					</div>
 				</div>
 				<div class="form-row">
 					<div class="form-group col-md-6">
 						<label>Telefono 1:</label>
-						<input name="tel1-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 1" required>
+						<?php
+							if (isset($_GET['pTel']))
+							{
+								$pTel = $_GET['pTel'];
+								echo '<input name="tel1-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 1" value="'.$pTel.'" required>';
+							}
+							else
+							{
+								echo '<input name="tel1-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 1" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 						</div>
 					</div>
 					<div class="form-group col-md-6">
 						<label>Telefono 2:</label>
-						<input name="tel2-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 2">
+						<?php
+							if (isset($_GET['sTel']))
+							{
+								$sTel = $_GET['sTel'];
+								echo '<input name="tel2-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 2" value="'.$sTel.'">';
+							}
+							else
+							{
+								echo '<input name="tel2-input" type="text" class="form-control" pattern="(\(\+[0-9]{3}\))?[0-9]{8}[0-9]*" placeholder="Telefono 2">';
+							}
+						?>
 					</div>
 				</div>
 				<div class="form-row">
@@ -116,7 +215,17 @@ include_once('scripts/conexion.inc');
 					</div>
 					<div class="form-group col-md-6">
 						<label>Nombre de usuario:</label>
-						<input name="username-input" type="text" class="form-control" placeholder="Nombre de usario" required>
+						<?php
+							if (isset($_GET['usuario']))
+							{
+								$usuario = $_GET['usuario'];
+								echo '<input name="username-input" type="text" class="form-control" placeholder="Nombre de usario" value="'.$usuario.'" required>';
+							}
+							else
+							{
+								echo '<input name="username-input" type="text" class="form-control" placeholder="Nombre de usario" required>';
+							}
+						?>
 						<div class="invalid-feedback">
 							Por favor, ingrese información válida.
 						</div>
@@ -146,6 +255,22 @@ include_once('scripts/conexion.inc');
 						<label class="custom-file-label" for="customFile">Seleccione una imagen</label>
 					</div>
 				</div>
+
+				<?php
+					if (isset($_GET['signup']))
+					{
+						echo '<div class="form row justify-content-center" style="margin: 10% 0 0 0">';
+						echo '<div class="alert alert-danger" role="alert">';
+						$signUpError = $_GET['signup'];
+
+						if ($signUpError == 'duplicate')
+							echo 'La cedula ingresada ya existe.';
+						elseif ($signUpError == 'usernameTaken')
+							echo 'El nombre de usuario ingresado ya existe.';
+						echo '</div>';
+						echo '</div>';
+					}
+				?>
 				<div class="form row justify-content-center" style="margin: 10%">
 					<button id="btnSignIn" class="btn my-2 my-sm-0 btn-dark" style="margin: 0 1%" form="myform" type="submit" name="submit">
 						Sign Up
