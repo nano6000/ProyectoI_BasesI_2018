033f47fdf34700fca6449455167bafef7e3d4a6a
kalmas77 <11956777+kalmas77@users.noreply.github.com>
2018-05-13 18:43:39 -0600

    triggers de update añadidos

--- Scripts/4_Crear_Triggers_y_secuencias.sql
+++ Scripts/4_Crear_Triggers_y_secuencias.sql
@@ -498,3 +498,326 @@ BEGIN
     :new.usuario_ultima_modificacion:=USER;
 END;
 /
+/*Triggers de update*/
+CREATE OR REPLACE TRIGGER abono_codigo_trg_upd BEFORE
+    UPDATE ON abono
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER arbol_auditoria_upd BEFORE
+    UPDATE ON arbol
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER calificacionhuerta_codigocalif_upd BEFORE
+    UPDATE ON calificacionhuerta
+    FOR EACH ROW
+    WHEN ( new.codigocalificacion IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER cambioclave_codigo_trg_upd BEFORE
+    UPDATE ON cambioclave
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER canton_codigo_trg_upd BEFORE
+    UPDATE ON canton
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER clase_codigoclase_trg_upd BEFORE
+    UPDATE ON clase
+    FOR EACH ROW
+    WHEN ( new.codigoclase IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER colaborador_codigocolaborador_upd BEFORE
+    UPDATE ON colaborador
+    FOR EACH ROW
+    WHEN ( new.codigocolaborador IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER colaboradorhuerta_auditoria_upd BEFORE
+    UPDATE ON colaboradorhuerta
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER color_codigocolor_trg_upd BEFORE
+    UPDATE ON color
+    FOR EACH ROW
+    WHEN ( new.codigocolor IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER distrito_codigo_trg_upd BEFORE
+    UPDATE ON distrito
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER division_codigodivision_trg_upd BEFORE
+    UPDATE ON division
+    FOR EACH ROW
+    WHEN ( new.codigodivision IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER emailpersona_auditoria_trg_upd BEFORE
+    UPDATE ON emailpersona
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER especie_codigoespecie_trg_upd BEFORE
+    UPDATE ON especie
+    FOR EACH ROW
+    WHEN ( new.codigoespecie IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER familia_codigofamilia_trg_upd BEFORE
+    UPDATE ON familia
+    FOR EACH ROW
+    WHEN ( new.codigofamilia IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER genero_codigogenero_trg_upd BEFORE
+    UPDATE ON genero
+    FOR EACH ROW
+    WHEN ( new.codigogenero IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER hortaliza_codigoplanta_trg_upd BEFORE
+    UPDATE ON hortaliza
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER huerta_codigohuerta_trg_upd BEFORE
+    UPDATE ON huertaA
+    FOR EACH ROW
+    WHEN ( new.codigohuerta IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER localizacion_codigo_trg_upd BEFORE
+    UPDATE ON localizacion
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER orden_codigoorden_trg_upd BEFORE
+    UPDATE ON orden
+    FOR EACH ROW
+    WHEN ( new.codigoorden IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER pais_codigo_trg_upd BEFORE
+    UPDATE ON pais
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER persona_trg BEFORE
+    UPDATE ON persona
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER planta_codigoplanta_trg_upd BEFORE
+    UPDATE ON planta
+    FOR EACH ROW
+    WHEN ( new.codigoplanta IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER plantahuerta_auditoria_trg_upd BEFORE
+    UPDATE ON plantahuerta
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER propiedad_codigopropiedad_trg_upd BEFORE
+    UPDATE ON propiedad
+    FOR EACH ROW
+    WHEN ( new.codigopropiedad IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER propiedadhor_auditoria_trg_upd BEFORE
+    UPDATE ON propiedadhortaliza
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER provincia_codigo_trg_upd BEFORE
+    UPDATE ON provincia
+    FOR EACH ROW
+    WHEN ( new.codigo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER telefonopersona_auditoria_trg_upd BEFORE
+    UPDATE ON telefonopersona
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER tipocambium_codigocambium_trg_upd BEFORE
+    UPDATE ON tipocambium
+    FOR EACH ROW
+    WHEN ( new.codigocambium IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER tipocorteza_codigocorteza_trg_upd BEFORE
+    UPDATE ON tipocorteza
+    FOR EACH ROW
+    WHEN ( new.codigocorteza IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER tipohortaliza_codigotipo_trg_upd BEFORE
+    UPDATE ON tipohortaliza
+    FOR EACH ROW
+    WHEN ( new.codigotipo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER tipotransaccion_codigo_trg_upd BEFORE
+    UPDATE ON tipotransaccion
+    FOR EACH ROW
+    WHEN ( new.codigotipo IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER tipoxilema_codigoxilema_trg_upd BEFORE
+    UPDATE ON tipoxilema
+    FOR EACH ROW
+    WHEN ( new.codigoxilema IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER transaccion_codigotransaccion_upd BEFORE
+    UPDATE ON transaccion
+    FOR EACH ROW
+    WHEN ( new.codigotransaccion IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER usuario_trg BEFORE
+    UPDATE ON usuario
+    FOR EACH ROW
+BEGIN
+	:new.fec_ultima_modificacion:=SYSDATE;
+    :new.usuario_ultima_modificacion:=USER;
+END;
+/
+
+CREATE OR REPLACE TRIGGER vitamina_codigovitamina_trg_upd BEFORE
+    UPDATE ON vitamina
+    FOR EACH ROW
+    WHEN ( new.codigovitamina IS NULL )
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER vitaminacolor_auditoria_trg_upd BEFORE
+    UPDATE ON vitaminacolor
+    FOR EACH ROW
+BEGIN
+    :new.fec_ultima_modificacion := SYSDATE;
+    :new.usuario_ultima_modificacion := USER;
+END;
+/
+CREATE OR REPLACE TRIGGER vitaminacolor_trg_upd BEFORE
+    UPDATE ON vitaminacolor
+    FOR EACH ROW
+BEGIN
+	:new.fec_ultima_modificacion:=SYSDATE;
+    :new.usuario_ultima_modificacion:=USER;
+END;
+/
